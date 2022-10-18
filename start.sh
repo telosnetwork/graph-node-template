2107c0b665ffe23621cb67b643881b11983453e2
Jesse Schulman <jesse@caleos.io>
2022-10-18 11:33:46 -0700

    initial commit

---
+++ start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+INSTALL_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+. $INSTALL_ROOT/.env
+
+LOG_NAME="$(basename $INSTALL_ROOT)"
+LOG_PATH="$INSTALL_ROOT/$LOG_NAME.log"
+
+CMD=cargo run -p graph-node --release -- \
+  --postgres-url postgresql://$PG_USER:$PG_PASS@$PG_HOST:$PG_PORT/$PG_DB_NAME \
+  --ethereum-rpc $RPC_NAME:$RPC_URL \
+  --ipfs $IPFS_HOST:$IPFS_PORT
+
+nohup $CMD >> "$LOG_PATH" 2>&1 &
+PID="$!"
+echo "graph-node started with pid $PID"
+echo $PID > $INSTALL_ROOT/graph-node.pid
